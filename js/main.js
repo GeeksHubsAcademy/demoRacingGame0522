7a722dd7e64e7a1dd29e00b96700b0bda1fb27e7
Dave86dev <davidochando1986@gmail.com>
2022-05-26 10:57:14 +0200

    Preparado para implementar pantalla de selección

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 
-const screenSwitch = (pantallaActual,pantallaDestino) => {
+const screenSwitch = (pantallaDestino) => {
 
-    let actual = document.getElementById(pantallaActual);
+    // let actual = document.getElementById(pantallaActual);
     let destino = document.getElementById(pantallaDestino);
-
-    destino.style.display = "block";
-    actual.style.display = "none";
+    destino.style.display = "flex";
+    // actual.style.display = "none";
     
     // if(numeroPantalla == "pantalla2"){
     //     document.getElementById("pantalla1").style.display = "none";
@@ -14,5 +13,13 @@ const screenSwitch = (pantallaActual,pantallaDestino) => {
     //     document.getElementById("pantalla2").style.display = "none";
     //     destino.style.display = "block";
     // };
+
+    let arrayPantallas = ["pantalla1","pantalla2","pantalla3","pantalla4","pantalla5"];
+
+    for(let pantalla of arrayPantallas){
+        if(pantalla != pantallaDestino){
+            document.getElementById(pantalla).style.display = "none";
+        };
+    };
    
 };
