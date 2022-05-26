9bed570216d501f69194b024d8c8cd008c04d277
Dave86dev <davidochando1986@gmail.com>
2022-05-26 10:38:15 +0200

    Multiple cambio de screen, lineal progresivo

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 
-const screenSwitch = (numeroPantalla) => {
+const screenSwitch = (pantallaActual,pantallaDestino) => {
 
-    let destino = document.getElementById(numeroPantalla);
+    let actual = document.getElementById(pantallaActual);
+    let destino = document.getElementById(pantallaDestino);
 
-
-    if(numeroPantalla == "pantalla2"){
-        document.getElementById("pantalla1").style.display = "none";
-        destino.style.display = "block";
-    } else {
-        document.getElementById("pantalla2").style.display = "none";
-        destino.style.display = "block";
-    };
+    destino.style.display = "block";
+    actual.style.display = "none";
+    
+    // if(numeroPantalla == "pantalla2"){
+    //     document.getElementById("pantalla1").style.display = "none";
+    //     destino.style.display = "block";
+    // } else {
+    //     document.getElementById("pantalla2").style.display = "none";
+    //     destino.style.display = "block";
+    // };
    
 };
